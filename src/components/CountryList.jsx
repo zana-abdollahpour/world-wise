c7c7aed8f9985fc5281ac798414e4a8d60d5357a
zana-abdollahpour <zana.ap4@gmail.com>
2023-07-25 14:15:14 +0330

    implement countries list

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import Spinner from "./Spinner";
+import Message from "./Message";
+import CountryItem from "./CountryItem";
+
+import styles from "./CountryList.module.css";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map." />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem key={country.id} country={country} />
+      ))}
+    </ul>
+  );
+}
+export default CountryList;
