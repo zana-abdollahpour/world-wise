3fb28f9ebed6fe5f5ec5918fffa696c0c26a85d3
zana-abdollahpour <zana.ap4@gmail.com>
2023-07-25 14:12:09 +0330

    change some dev data

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 
-const BASE_URL = `http://localhost:8000`;
+const BASE_URL = `http://localhost:9000`;
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -49,7 +50,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>List of countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
