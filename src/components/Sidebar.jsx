04b9d44598580aae88b6be9da26b4960c7002d8e
zana-abdollahpour <zana.ap4@gmail.com>
2023-07-25 11:20:20 +0330

    add nested routes for /app/* URL

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
@@ -9,9 +11,7 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-
-      <p>List of cities</p>
-
+      <Outlet />
       <Footer />
     </div>
   );
