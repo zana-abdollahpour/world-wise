0037db3b126c0674e8e2870114faaf9b294d8de1
zana-abdollahpour <zana.ap4@gmail.com>
2023-07-25 09:10:35 +0330

    add main pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
-import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
