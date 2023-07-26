cca4b92d54a0d9a80f9da32d6397bf6d9426be48
zana-abdollahpour <zana.ap4@gmail.com>
2023-07-26 10:46:52 +0330

    add programmatic nav to /app/cities in /app index

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
@@ -44,10 +44,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate to="cities" replace />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
