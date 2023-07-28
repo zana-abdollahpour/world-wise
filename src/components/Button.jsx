60b8ea217df40ad514a1c62d9c7e1e2a0941f576
zana-abdollahpour <zana.ap4@gmail.com>
2023-07-28 09:49:37 +0330

    change main element from div to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,9 @@ import styles from "./Button.module.css";
 
 function Button({ children, onClick, type }) {
   return (
-    <div className={`${styles.btn} ${styles[type]}`} onClick={onClick}>
+    <button className={`${styles.btn} ${styles[type]}`} onClick={onClick}>
       {children}
-    </div>
+    </button>
   );
 }
 export default Button;
